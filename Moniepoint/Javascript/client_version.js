6b14e92b927a92fa7ccca736e87ca9467f3f7728
niuniujun <niuniujun@argun.cc>
2024-06-11 01:02:50 +0800

    update

--- Moniepoint/Javascript/client_version.js
+++ Moniepoint/Javascript/client_version.js
@@ -3,10 +3,10 @@ const headers = $request.headers; // 获取原始请求
 try {
     if (headers["client-version"] && headers["banking-domain"] === "PERSONAL") {
         // 如果存在client-version和banking-domain，且banking-domain为PERSONAL
-        headers["client-version"] = "1.6.4"; // 更新client-version为1.6.4
+        headers["client-version"] = "1.6.5"; // 更新client-version为1.6.4
     } else if (headers["client-version"]) {
         // 如果只存在client-version
-        headers["client-version"] = "2.5.4"; // 更新client-version为2.5.4
+        headers["client-version"] = "2.5.5"; // 更新client-version为2.5.4
     }
 
     // 重新设置修改后的请求头部
